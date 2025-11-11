1691ef326c0eabf4e0ce5cac0bf5104797ed3c8b
marc20197 <142219074+marc20197@users.noreply.github.com>
2025-11-11 09:37:25 +0100

    Primer codi de prova

---
+++ app.R
@@ -0,0 +1,47 @@
+#
+# Mapa incendis
+
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(ggplot2)
+library(DT)
+library(sf)
+
+# Exemple: llegir un fitxer SHP
+temp_dir <- tempdir()
+unzip("incendis94.zip", exdir = temp_dir)
+
+# Busca el fitxer .shp dins el zip
+temp_dir <- tempdir()
+getwd()
+list.files()
+unzip("incendis94.zip", exdir = temp_dir)
+
+# Busca el fitxer .shp dins el zip
+shp_file <- list.files(temp_dir, pattern = "\\.shp$", full.names = TRUE)
+
+# Llegeix-lo amb sf
+shape <- st_read(shp_file)
+
+# Converteix a coordenades geogràfiques (lat/lon)
+shape <- st_transform(shape, crs = 4326)
+# shape <- readRDS("shape_simple.rds")
+ui <- fluidPage(
+  titlePanel("Mapa d'incendis de Catalunya (1994)"),
+  
+  leafletOutput("mapa", height = 600)
+)
+server <- function(input, output, session) {
+  
+  output$mapa <- renderLeaflet({
+    leaflet(shape) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolygons(
+        color = "red",
+        weight = 1,
+        fillOpacity = 0.4
+      )
+  })
+}
+shinyApp(ui = ui, server = server)
